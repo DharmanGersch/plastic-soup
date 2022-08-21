f81a28998abe2a5581f203bd476979888dc6270e
Dharman Gersch <dharman.gersch@gmail.com>
2022-08-21 21:33:18 +1000

    changes made on the mac via ssh

--- playing.py
+++ playing.py
@@ -1,5 +1,30 @@
-from buildhat import Motor
+from buildhat import Motor, Light
+import time
 
 motor_a = Motor('A')
+light_b = Light('B')
+#motor_a.run_for_seconds(4, speed=100)
 
-motor_a.run_for_seconds(5)
+#this is a loop
+for i in range(10):
+    #everything indented like this will repeat
+    motor_a.run_for_degrees(90)
+    light_b.on()
+    time.sleep(1)
+    light_b.off()
+    print(motor_a.get_position())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
